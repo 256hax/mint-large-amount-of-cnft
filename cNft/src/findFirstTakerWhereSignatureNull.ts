f95b8ddc410c79eea42b04a7436be40c8a3124ce
256hax <256hax@gmail.com>
2024-02-12 22:00:26 +0900

    feat: add return value

--- cNft/src/findFirstTakerWhereSignatureNull.ts
+++ cNft/src/findFirstTakerWhereSignatureNull.ts
@@ -3,26 +3,22 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const findFirstTakerWhereSignatureNull = async () => {
-  const taker = await prisma.taker.findFirst({
-    where: {
-      OR: [
-        { signature: null }, // null supports null or ''
-      ]
-    },
-    orderBy: {
-      id: 'asc',
-    },
-  });
+  try {
+    const taker = await prisma.taker.findFirst({
+      where: {
+        OR: [
+          { signature: null }, // null supports null or ''
+        ],
+      },
+      orderBy: {
+        id: 'asc',
+      },
+    });
 
-  console.log(taker);
-}
-
-findFirstTakerWhereSignatureNull()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    // console.log(taker);
+    return taker;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
