54544a558083507d429454726ff74610a12ba898
256hax <256hax@gmail.com>
2024-02-13 00:26:24 +0900

    dev: add handling prisma

--- cNft/src/mintWithoutCollection.ts
+++ cNft/src/mintWithoutCollection.ts
@@ -1,8 +1,12 @@
 // Lib
 import * as dotenv from 'dotenv';
 import * as bs58 from 'bs58';
-import fs from 'fs';
-import { sleep } from '../../lib/sleep';
+import { sleep } from './lib/sleep';
+
+// Prisma
+import { countTakersWhereSignatureNull } from './countTakersWhereSignatureNull';
+import { findFirstTakerWhereSignatureNull } from './findFirstTakerWhereSignatureNull';
+import { updateSignatureToDB } from './updateSignatureToDB';
 
 // Metaplex
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
@@ -10,68 +14,79 @@ import { keypairIdentity, none, publicKey } from '@metaplex-foundation/umi';
 import { mintV1 } from '@metaplex-foundation/mpl-bubblegum';
 
 const mintWithoutCollection = async () => {
-  // Taker List
-  const takerListText = fs.readFileSync('./src/assets/takerList.txt', 'utf-8');
-  const takerListArray = takerListText.split(/\n/);
-
-  // ----------------------------------------------------
-  //  Mint cNFT to Taker
-  // ----------------------------------------------------
-  console.log('leafOwner, Signature');
-
   try {
-    const mint = async () => {
-      for (const taker of takerListArray) {
-        // ----------------------------------------------------
-        //  Setup
-        // ----------------------------------------------------
-        dotenv.config();
+    let numberOfTakers = await countTakersWhereSignatureNull();
+    console.log('Starting numberOfTakers:', numberOfTakers);
 
-        // Set Endpoint
-        const endpoint = process.env.ENDPOINT;
-        if (!endpoint) throw new Error('endpoint not found.');
+    while (numberOfTakers > 0) {
+      // ----------------------------------------------------
+      //  Setup
+      // ----------------------------------------------------
+      dotenv.config();
 
-        const umi = createUmi(endpoint);
+      // Set Endpoint
+      const endpoint = process.env.ENDPOINT;
+      if (!endpoint) throw new Error('endpoint not found.');
 
-        // Set Payer
-        const payerSecretKey = process.env.PAYER_SECRET_KEY;
-        if (!payerSecretKey) throw new Error('payerSecretKey not found.');
+      const umi = createUmi(endpoint);
 
-        const secretKeyUInt8Array = new Uint8Array(JSON.parse(payerSecretKey));
-        const payerKeypair =
-          umi.eddsa.createKeypairFromSecretKey(secretKeyUInt8Array);
+      // Set Payer
+      const payerSecretKey = process.env.PAYER_SECRET_KEY;
+      if (!payerSecretKey) throw new Error('payerSecretKey not found.');
 
-        umi.use(keypairIdentity(payerKeypair));
+      const secretKeyUInt8Array = new Uint8Array(JSON.parse(payerSecretKey));
+      const payerKeypair =
+        umi.eddsa.createKeypairFromSecretKey(secretKeyUInt8Array);
 
-        // ----------------------------------------------------
-        //  Minting without a Collection
-        // ----------------------------------------------------
-        // Replace to your Merkle Tree.
-        const merkleTree = publicKey(
-          'AxR2UTtq3pZ5ZFaAs9tSWtiz4HftTAftKzMfs8ZjtcqQ'
-        );
+      umi.use(keypairIdentity(payerKeypair));
 
-        const result = await mintV1(umi, {
-          leafOwner: publicKey(taker),
-          merkleTree,
-          metadata: {
-            name: 'My cNFT',
-            uri: 'https://arweave.net/ZWOU6VbcltnL22QJQJAQ8hoojgs_fYHxXLE76khXt0c',
-            sellerFeeBasisPoints: 500, // 500 = 5%
-            collection: none(),
-            creators: [
-              { address: umi.identity.publicKey, verified: true, share: 100 },
-            ],
-          },
-        }).sendAndConfirm(umi);
+      console.log(1);
+      // ----------------------------------------------------
+      //  Minting without a Collection
+      // ----------------------------------------------------
+      // Get Taker Address from DB.
+      const takerInfo = await findFirstTakerWhereSignatureNull();
+      if (!takerInfo) throw new Error('Not found takerInfo.');
 
-        const signature = bs58.encode(result.signature);
-        console.log(`${taker}, ${signature}`);
+      // Replace to your Merkle Tree.
+      const merkleTree = publicKey(
+        'AxR2UTtq3pZ5ZFaAs9tSWtiz4HftTAftKzMfs8ZjtcqQ'
+      );
 
+      console.log(2);
+      const mintingResult = await mintV1(umi, {
+        leafOwner: publicKey(takerInfo.address),
+        merkleTree,
+        metadata: {
+          name: 'My cNFT',
+          uri: 'https://arweave.net/ZWOU6VbcltnL22QJQJAQ8hoojgs_fYHxXLE76khXt0c',
+          sellerFeeBasisPoints: 500, // 500 = 5%
+          collection: none(),
+          creators: [
+            { address: umi.identity.publicKey, verified: true, share: 100 },
+          ],
+        },
+      }).sendAndConfirm(umi);
+
+      console.log(3);
+      // console.log minting result.
+      const signature = bs58.encode(mintingResult.signature);
+      console.log(`${takerInfo.id}, ${takerInfo.address}, ${signature}`);
+
+      // Update signature to DB.
+      const updatedResult = await updateSignatureToDB(takerInfo.id, signature);
+
+      console.log(4);
+      // If the update successful.
+      if (updatedResult.signature) {
         await sleep(300);
+
+        numberOfTakers = await countTakersWhereSignatureNull();
+        console.log('Updated numberOfTakers:', numberOfTakers);
+      } else {
+        throw new Error('updatedResult.signature is null.');
       }
-    };
-    mint();
+    }
   } catch (e) {
     console.log(e);
   }
