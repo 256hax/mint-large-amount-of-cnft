172373f725e6d2d6f30d5fea6c23f5caf5fccfa2
256hax <256hax@gmail.com>
2024-02-12 22:24:24 +0900

    ref: remove unused code

--- cNft/src/findAllTakers.ts
+++ cNft/src/findAllTakers.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const main = async () => {
+const findAllTakers = async () => {
   const taker = await prisma.taker.findMany({
     orderBy: {
       id: 'asc',
@@ -12,12 +12,4 @@ const main = async () => {
   console.log(taker);
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+findAllTakers();
