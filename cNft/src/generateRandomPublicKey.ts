f4cd8eab5490eb4e5ddcabb9d5b2d7566deff50e
256hax <256hax@gmail.com>
2024-02-24 00:15:24 +0900

    chore: change default number

--- cNft/src/generateRandomPublicKey.ts
+++ cNft/src/generateRandomPublicKey.ts
@@ -1,6 +1,6 @@
 import { Keypair } from '@solana/web3.js';
 
-const numberOfGeneratePublicKey = 10;
+const numberOfGeneratePublicKey = 100;
 
 const generateRandomPublicKey = async () => {
   for (let i = 0; i < numberOfGeneratePublicKey; i++) {
